f2dac47b3cdfea56187ea9ad4a7acaf0f0a2f218
olka276 <aleksandra.k376@gmail.com>
2020-03-18 20:55:18 +0100

    first release

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-var http = require('http');
-var PORT = process.env.port || 4001;
-var fs = require('fs');
-var io = require('socket.io').listen(http);
-
-http.createServer(function(request,response){
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('Listening on port '+PORT)
-}).listen(PORT, () => console.log(`Legancko na porcie ${PORT}`));
+const io = require('socket.io')(4001);
+const users={};
 
 
-io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-      console.log(data);
-    });
-  });
-// io.on("connection", function(socket) {
-//     socket.on('connected', function(data) {
-//         socket.broadcast.emit('connected', data);
-//     });
-// });
+io.on('connection', socket => {
+    console.log("Server is listening on port 4001. New user has been connected.");
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        console.log(users[socket.id]+" connected")
+        socket.broadcast.emit('user-connected', name);
+    })
+    socket.on('send-chat-message', message => {
+      socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})
+    })
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user-disconnected', users[socket.id]);
+      console.log(users[socket.id]+" disconnected");
+      delete users[socket.id];
+  })
+})
